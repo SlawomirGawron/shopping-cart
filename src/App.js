bc526100bef50683162ec1c248b3cf7d08831a39
Slawomir Gawron <slawomirgawron8@gmail.com>
2019-05-03 15:50:13 -0400

    VCS check.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import MainPage from './components/mainPage/MainPage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Main Page</h1>
+      <MainPage />
     </div>
   );
 }
---
+++ src/components/mainPage/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MainPage.scss';
+
+function MainPage(props) {
+    return (
+        <div className="App">
+            <h1>Main Page</h1>
+        </div>
+    );
+}
+
+export default MainPage;
